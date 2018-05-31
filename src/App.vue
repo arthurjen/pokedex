bdb984518250d5d8e121f03053f2f521190de26b
Rehmsy <sarahrehmer@gmail.com>
2018-05-31 14:25:06 -0700

    added PokeHeader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Pokedex</h1>
+    <PokeHeader/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeHeader from './components/PokeHeader';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    PokeHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
