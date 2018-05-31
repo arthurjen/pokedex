529046ebc429876455f144ceb7a4ff0d261e2ad4
Rehmsy <sarahrehmer@gmail.com>
2018-05-31 14:51:07 -0700

    tiles display pokemon

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,26 @@
   <div id="app">
     <h1>Pokedex</h1>
     <PokeHeader/>
+    <Results :pokeList="pokemonList"/>
 
   </div>
 </template>
 
 <script>
 import PokeHeader from './components/PokeHeader';
+import Results from './components/Results';
+import pokemonList from '../pokemon.js';
 
 export default {
+  data() {
+    return {
+      pokemonList: pokemonList
+    }
+
+  },
   components: {
-    PokeHeader
+    PokeHeader,
+    Results
   }
 }
 </script>
--- src/components/PokeHeader.vue
+++ src/components/PokeHeader.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="header">
     <h2>Header</h2>
+    <PokeFilter/>
+    <PokeSort/>
+
   </div>
 </template>
 
 <script>
+import PokeFilter from './PokeFilter'
+import PokeSort from './PokeSort'
+
 export default {
+  components: {
+    PokeFilter, 
+    PokeSort
+  }
   
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
